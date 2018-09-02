b604083bcb4f4d358cee6e02e386d37541e31ec9
Matt Auretta <mauretta2@gmail.com>
2018-09-01 18:09:27 -0600

    testing auth

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,28 +12,6 @@
  //Create variable for database
  var database = firebase.database();
 
- // FirebaseUI config.
- var uiConfig = {
-     signInSuccessUrl: 'https://mattauretta.github.io/Train-Scheduler/',
-     signInOptions: [
-         // Leave the lines as is for the providers you want to offer your users.
-         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-     ],
-     // tosUrl and privacyPolicyUrl accept either url string or a callback
-     // function.
-     // Terms of service url/callback.
-     tosUrl: '<your-tos-url>',
-     // Privacy policy url/callback.
-     privacyPolicyUrl: function () {
-         window.location.assign('<your-privacy-policy-url>');
-     }
- };
-
- // Initialize the FirebaseUI Widget using Firebase.
- var ui = new firebaseui.auth.AuthUI(firebase.auth());
- // The start method will wait until the DOM is loaded.
- ui.start('#firebaseui-auth-container', uiConfig);
-
  //Button for adding trains
  $("#add-train").on("click", function (event) {
      event.preventDefault();
